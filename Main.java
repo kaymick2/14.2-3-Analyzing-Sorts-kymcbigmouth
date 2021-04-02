bdbab5249b4324f0accdf4b3e453c53d55538621
Kyle McDowell <kymcd21@icstudents.org>
2021-04-02 20:49:35 +0000

    FINAL

--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 /**
    This program measures how long it takes to sort an
@@ -15,15 +16,22 @@ public class Main
 
       // Construct random array
    
-      int[] a = ArrayUtil.randomIntArray(n, 100);
-      SelectionSorter sorter = new SelectionSorter(a);
+      int[] array = ArrayUtil.randomIntArray(n, 100);
       
       // Use stopwatch to time selection sort
       
       StopWatch timer = new StopWatch();
       
       timer.start();
-      sorter.sort();
+    for (int i = 0; i <= (array.length) - 1; i++) {
+      int element = array[i];
+      int j = i;
+      while (j > 0 && array[j - 1] > element) {
+        array[j] = array[j - 1];
+        j = j - 1;
+      }
+      array[j] = element;
+    }
       timer.stop();
       
       System.out.println("Elapsed time: " 
